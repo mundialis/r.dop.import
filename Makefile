ce3a891df5472d704507626669b847f54b3e449c
Anika Weinmann <weinmann@mundialis.de>
2023-04-21 07:30:16 +0000

    r.dop.import tests

--- Makefile
+++ Makefile
@@ -2,9 +2,9 @@ MODULE_TOPDIR = ../..
 
 PGM = r.dop.import
 
-ETCFILES = download_urls
+ETCFILES = download_urls federal_states
 
-include $(MODULE_TOPDIR)/include/Make/Python.make
 include $(MODULE_TOPDIR)/include/Make/Script.make
+include $(MODULE_TOPDIR)/include/Make/Python.make
 
 default: script
--- download_urls.py
+++ download_urls.py
@@ -1,35 +1,25 @@
 URLS = {
-    "Brandenburg":
-        "https://github.com/mundialis/tile-indices/raw/main/brandenburg/"
-        "geoportalBB_DOP20_tileindex.gpkg.gz",
-    "Berlin":
-        "https://github.com/mundialis/tile-indices/raw/main/brandenburg/"
-        "geoportalBB_DOP20_tileindex.gpkg.gz",
-    "Baden-Württemberg":
-        None,
-    "Bayern":
-        None,
-    "Bremen":
-        None,
-    "Hessen":
-        None,
-    "Hamburg":
-        None,
-    "Mecklenburg-Vorpommern":
-        None,
-    "Niedersachsen":
-        None,
-    "Nordrhein-Westfalen":
-        "https://github.com/mundialis/openNRW/raw/master/dop/"
-        "openNRW_DOP10_tileindex.gpkg.gz",
-    "Rheinland-Pfalz":
-        None,
-    "Schleswig-Holstein":
-        None,
-    "Saarland":
-        None,
-    "Sachsen":
-        None,
-    "Sachsen-Anhalt":
-        None
+    "Brandenburg": "https://github.com/mundialis/tile-indices/raw/main/DOP/"
+    "BE_BB/geoportalBB_DOP20_tileindex.gpkg.gz",
+    "Berlin": "https://github.com/mundialis/tile-indices/raw/main/brandenburg/"
+    "geoportalBB_DOP20_tileindex.gpkg.gz",
+    "Baden-Württemberg": None,
+    "Bayern": None,
+    "Bremen": None,
+    "Hessen": None,
+    "Hamburg": None,
+    "Mecklenburg-Vorpommern": None,
+    "Niedersachsen": None,
+    "Nordrhein-Westfalen": "https://github.com/mundialis/openNRW/raw/master/"
+    "dop/openNRW_DOP10_tileindex.gpkg.gz",
+    "Rheinland-Pfalz": None,
+    "Schleswig-Holstein": None,
+    "Saarland": None,
+    "Sachsen": None,
+    "Sachsen-Anhalt": None,
 }
+
+WMS_HE = (
+    "https://www.gds-srv.hessen.de/cgi-bin/lika-services/ogc-free-"
+    "images.ows?language=ger&"
+)
