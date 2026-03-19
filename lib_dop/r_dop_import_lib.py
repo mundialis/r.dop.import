f6c963ab6d78fc8763bc6e5d2789ffad900ab5e5
linakrisztian <106728040+linakrisztian@users.noreply.github.com>
2026-03-19 16:37:06 +0100

    Update lib_dop/r_dop_import_lib.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- lib_dop/r_dop_import_lib.py
+++ lib_dop/r_dop_import_lib.py
@@ -437,7 +437,7 @@ def import_and_reproject(
 
     tmp_loc = None
     if epsg != int(
-        grass.parse_command("g.proj", flags="g")["srid"].split(":")[1]
+        grass.parse_command("g.proj", flags="g")["srid"].split(":")[1],
     ):
         aoi_map_to_set_region1 = aoi_map
         # get actual location, mapset, ...
